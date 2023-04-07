deb4dbba51d2fce05b6eba297ffbfb119bfc0204
bhaktiprabhu <65298221+bhaktiprabhu@users.noreply.github.com>
2023-04-08 00:56:08 +0200

    Py File added for reading CSV and storing in DataFrame

---
+++ py_files/Spark_Read_CSV.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import pyspark 
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
+
+spark = SparkSession.builder.getOrCreate()
+
+flightSchema = StructType() \
+    .add("YEAR", "integer")\
+    .add("QUARTER", "integer")\
+    .add("MONTH", "integer")\
+    .add("DAY_OF_MONTH", "integer")\
+    .add("DAY_OF_WEEK", "integer")\
+    .add("FL_DATE", "timestamp")\
+    .add("OP_UNIQUE_CARRIER", "string")\
+    .add("TAIL_NUM", "string")\
+    .add("OP_CARRIER_FL_NUM", "integer")\
+    .add("ORIGIN_AIRPORT_ID", "integer")\
+    .add("ORIGIN_AIRPORT_SEQ_ID", "integer")\
+    .add("ORIGIN_CITY_MARKET_ID", "integer")\
+    .add("ORIGIN", "string")\
+    .add("ORIGIN_CITY_NAME", "string")\
+    .add("ORIGIN_STATE_NM", "string")\
+    .add("DEST_AIRPORT_ID", "integer")\
+    .add("DEST_AIRPORT_SEQ_ID", "integer")\
+    .add("DEST_CITY_MARKET_ID", "integer")\
+    .add("DEST", "string")\
+    .add("DEST_CITY_NAME", "string")\
+    .add("DEST_STATE_NM", "string")\
+    .add("DEP_DELAY", "double")\
+    .add("DEP_DELAY_NEW", "double")\
+    .add("DEP_DEL15", "double")\
+    .add("ARR_DELAY", "double")\
+    .add("ARR_DELAY_NEW", "double")\
+    .add("ARR_DEL15", "double")\
+    .add("CANCELLED", "double")\
+    .add("CANCELLATION_CODE", "string")\
+    .add("DIVERTED", "double")\
+    .add("AIR_TIME", "double")\
+    .add("DISTANCE", "double")\
+    .add("DISTANCE_GROUP", "integer")\
+    .add("CARRIER_DELAY", "double")\
+    .add("WEATHER_DELAY", "double")\
+    .add("NAS_DELAY", "double")\
+    .add("SECURITY_DELAY", "double")\
+    .add("LATE_AIRCRAFT_DELAY", "double")
+
+flight_data=spark.read.csv("hdfs://namenode:9000/2022-12.csv", schema=flightSchema)
+flight_data.show(5)
+
+flight_data.printSchema()
